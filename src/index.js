1142176f29811aec45afdca70bf46b0c916d728d
rastapf <rupertstapf@hotmail.com>
2020-08-11 23:47:25 -0300

    Arquivo de entrada do React definido (mensagem de teste apenas)

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Index() {
+    return <div>Configuração inicial do React ok, aguardando desenvolvimento posterior.</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
